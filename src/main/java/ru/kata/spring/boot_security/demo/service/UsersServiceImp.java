ab84b909831f2c783a35804985842bf8cbdea868
tankercode <tanker1609dd@gmail.com>
2024-08-05 21:49:45 +0700

    правки 2

--- src/main/java/ru/kata/spring/boot_security/demo/service/UsersServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UsersServiceImp.java
@@ -1,9 +1,6 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
